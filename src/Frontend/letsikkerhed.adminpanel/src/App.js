5d9c4459659ffe505a950fe572ecf3efe54c6fc2
AndersJungdal <anders.jungdal@gmail.com>
2026-06-03 22:06:32 +0200

    alj ps test2

--- src/Frontend/letsikkerhed.adminpanel/src/App.js
+++ src/Frontend/letsikkerhed.adminpanel/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ALJ TEST
+          Learn React ALJ TEST 2
         </a>
       </header>
     </div>
